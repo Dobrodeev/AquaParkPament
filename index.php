a4511a8a418aa5b8b3e18752f9c80ab9ab2ec321
dobrodeev <dobrod@meta.ua>
2018-12-25 12:12:26 +0200

    добавили несколько переходов на другие страницы .php в input

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
 </head>
 <body>
+<script src="assets/jquery-3.2.1.js"></script>
+<script src="assets/js/bootstrap.js"></script>
 <a href="once_card.php">Купить разовую</a><br>
-<a href="abonement.php">Управление клубными картами</a><br>
-<a href="">Вход</a><br>
+<a href="clubCard.php">Управление клубными картами</a><br>
+<a href="comeInAqua.php">Вход</a><br>
 <a href="">Выход из</a><br>
-<a href="">Статистика</a><br>
+<a href="statistics.php">Статистика</a><br>
 
 </body>
 </html>
