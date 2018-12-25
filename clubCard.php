87469e1b129bbbd9da32affb2d32f1ead420861c
dobrodeev <dobrod@meta.ua>
2018-12-25 12:14:51 +0200

    создали переходы на Вход по клубной карте, Вход в зону аквапарка & Переход на статистику

---
+++ clubCard.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<script src="assets/jquery-3.2.1.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<a href="index.php">На главную</a>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dobrodeev
+ * Date: 25.12.2018
+ * Time: 11:32
+ */
+?>
---
+++ comeInAqua.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<script src="assets/jquery-3.2.1.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<form action="" method="post">
+    <input type="text" name="hash_card" value="" placeholder="Номер карты">
+    <input type="submit">
+</form>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dobrodeev
+ * Date: 25.12.2018
+ * Time: 11:10
+ */
+// if() карта активна -> inuse
+// на выходе ввести 50 мин
+?>
