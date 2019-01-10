d1ce4f832c033d3256f51cf729eee4ca606bc71d
Dobrodeev <dobrod@meta.ua>
2019-01-10 10:00:55 +0200

    добавили форму на выходе из Аквапарка чтобы туда вводить время (минут), сколько там провели

--- ComeInAquaPark.php
+++ ComeInAquaPark.php
@@ -1,3 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<script src="assets/jquery-3.2.1.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<img src="template/images/1cb9f5296c7400988b86167e065f4701.jpg" alt="in AquaPark">
+<form action="" method="post">
+    <input type="text" name="total_time" value="" placeholder="всего минут"><br>
+    <input type="submit" value="Зайти в Аквапарк"><br>
+    <a href="index.php">На главную</a>
+</form>
+</body>
+</html>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +26,4 @@
  * Date: 10.01.2019
  * Time: 9:34
  */
+?>
