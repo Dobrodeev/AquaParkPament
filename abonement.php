ffdefb45736b4d74787027218ee5f26ef1770df4
Dobrodeev <dobrod@meta.ua>
2019-01-11 00:03:06 +0200

    убрали отладочные строки из обработчика файла comeInAqua.php

--- abonement.php
+++ abonement.php
@@ -7,14 +7,12 @@
  */
 if ($_POST['go'])
 {
-//    echo 'Nomber of card: '.$_POST['hash_card'];
     $hash_card = $_POST['hash_card'];
     include 'DB.php';
     $stmt = $pdo->query("SELECT id_card FROM hour_cards WHERE hash_card='$hash_card'");
     $name = $stmt->fetchAll();
     if ($name)
     {
-//        echo 'OK';
         header('Location: ComeInAquaPark.php');
     }
     else
