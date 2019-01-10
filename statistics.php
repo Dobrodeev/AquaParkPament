455dc9ae57ceb0fee7c16862f55485a181d5b041
Dobrodeev <dobrod@meta.ua>
2019-01-10 21:42:13 +0200

    Сделали заготовку для подсчета разных параметров для статитстики

--- statistics.php
+++ statistics.php
@@ -1,33 +1,30 @@
-<?
-header('Location: ComeInAquaPark.php');
-$x = 90;
-if ($x == 90)
-{
-    echo 'Come in';
-//    header('Location: ComeInAquaPark.php');
-}
-?>
-<!--<!doctype html>-->
-<!--<html lang="en">-->
-<!--<head>-->
-<!--    <meta charset="UTF-8">-->
-<!--    <meta name="viewport"-->
-<!--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">-->
-<!--    <meta http-equiv="X-UA-Compatible" content="ie=edge">-->
-<!--    <title>Statistics AquaPark</title>-->
-<!--    <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">-->
-<!--</head>-->
-<!--<body>-->
-<!--<script src="assets/jquery-3.2.1.js"></script>-->
-<!--<script src="assets/js/bootstrap.js"></script>-->
-<!--<a href="index.php">На главную</a>-->
-<!--</body>-->
-<!--</html>-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Statistics AquaPark</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<script src="assets/jquery-3.2.1.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<a href="index.php">На главную</a>
+</body>
+</html>
 <?php
 /**
  * Created by PhpStorm.
  * User: dobrodeev
  * Date: 25.12.2018
  * Time: 11:31
+ */
+/**
+ * Сколько куплено разовых абонементов
+ * Сколько клубных карт и на какую сумму
+ * Сумарное количество проданых абонементов в месяц
+ * Статистика по месяцам
  */
 ?>
