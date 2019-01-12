fc83cae5b9649a0be79cbabdffc6c27406264bfc
Dobrodeev <dobrod@meta.ua>
2019-01-12 21:58:08 +0200

    подключили сессию, чтобы передавать время за которое заплатил посетитель

--- abonement.php
+++ abonement.php
@@ -5,12 +5,22 @@
  * Date: 19.12.2018
  * Time: 11:10
  */
+session_start();
 if ($_POST['go'])
 {
     $hash_card = $_POST['hash_card'];
     include 'DB.php';
-    $stmt = $pdo->query("SELECT id_card FROM hour_cards WHERE hash_card='$hash_card'");
+    $stmt = $pdo->query("SELECT id_card, time_card FROM hour_cards WHERE hash_card='$hash_card'");
+//    $time = $pdo->query("SELECT time_card FROM hour_cards WHERE hash_card='$hash_card'");
+//    $_SESSION['time']= $time;
+//    print_r($_SESSION);
     $name = $stmt->fetchAll();
+//    echo '<pre>';
+//    print_r($name);
+//    echo '</pre>';
+    $_SESSION['time'] = $name[0]['time_card'];
+//    print_r($_SESSION);
+
     if ($name)
     {
         header('Location: ComeInAquaPark.php');
@@ -19,4 +29,5 @@ if ($_POST['go'])
     {
         echo 'Else...';
     }
+    /*** */
 }
