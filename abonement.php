13755a01c43ebe04fa58b5b1ab846f6989bcd2cd
Dobrodeev <dobrod@meta.ua>
2019-01-13 18:56:58 +0200

    добавили условие для входа в зону для клубных карт (запрос к БД теперь к другой таблице - club_cards)

--- abonement.php
+++ abonement.php
@@ -9,13 +9,32 @@ session_start();
 if ($_POST['go'])
 {
     $hash_card = $_POST['hash_card'];
+//    echo 'Hash: '.$hash_card.'<br>';
+    $firstSymbol = substr($hash_card, 0, 1);
+//    echo 'firstSymbol: '.$firstSymbol.'<br>';
     include 'DB.php';
-    $stmt = $pdo->query("SELECT id_card, time_card FROM hour_cards WHERE hash_card='$hash_card'");
-    $name = $stmt->fetchAll();
-    $_SESSION['time'] = $name[0]['time_card'];
-    if ($name)
+    if ($firstSymbol == 'a')
     {
-        header('Location: ComeInAquaPark.php');
+        $stmt = $pdo->query("SELECT id_card, time_card FROM hour_cards WHERE hash_card='$hash_card'");
+        $name = $stmt->fetchAll();
+        $_SESSION['time'] = $name[0]['time_card'];
+        if ($name)
+        {
+            header('Location: ComeInAquaPark.php');
+        }
+    }
+    elseif ($firstSymbol == 'b')
+    {
+        $stmt = $pdo->query("SELECT id_card, balance FROM club_cards WHERE hash_card='$hash_card'");
+        $name = $stmt->fetchAll();
+//        echo '<pre>';
+//        print_r($name);
+//        echo '</pre>';
+        $_SESSION['balance'] = $name[0]['balance'];
+        if ($name)
+        {
+            header('Location: ComeInAquaPark.php');
+        }
     }
     else
     {
