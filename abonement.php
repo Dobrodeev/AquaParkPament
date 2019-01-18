5bf798e9b0878c293070c6c7bb7d8b95d1c6fe44
Dobrodeev <dobrod@meta.ua>
2019-01-18 22:48:55 +0200

    ïðè âõîäå â àêâàïàðê òåïåðü ñòàòóñ êàðòû èçìåíÿåòñÿ: active->inuse, ïðåäîòâðàùåí âõîä â çîíó ñ óæå çàíÿòûõ êàðò

--- abonement.php
+++ abonement.php
@@ -9,35 +9,36 @@ session_start();
 if ($_POST['go'])
 {
     $hash_card = $_POST['hash_card'];
-//    echo 'Hash: '.$hash_card.'<br>';
     $firstSymbol = substr($hash_card, 0, 1);
-//    echo 'firstSymbol: '.$firstSymbol.'<br>';
+
     include 'DB.php';
     if ($firstSymbol == 'a')
     {
         $stmt = $pdo->query("SELECT id_card, time_card FROM hour_cards WHERE hash_card='$hash_card'");
         $name = $stmt->fetchAll();
-        $_SESSION['time'] = $name[0]['time_card'];
-        if ($name)
+        if ($name[0]['status_card'] == 'active')
         {
-            header('Location: ComeInAquaPark.php');
+            $stmt = $pdo->query("UPDATE hour_cards SET status_card='inuse' WHERE hash_card='$hash_card'");
+            $_SESSION['time'] = $name[0]['time_card'];
+            if ($name)
+            {
+                header('Location: ComeInAquaPark.php');
+            }
         }
     }
     elseif ($firstSymbol == 'b')
     {
         $stmt = $pdo->query("SELECT id_card, balance FROM club_cards WHERE hash_card='$hash_card'");
         $name = $stmt->fetchAll();
-//        echo '<pre>';
-//        print_r($name);
-//        echo '</pre>';
-        $_SESSION['balance'] = $name[0]['balance'];
-        if ($name)
+        if($name[0]['status_card'] == 'active')
         {
-            header('Location: ComeInAquaPark.php');
+            $stmt = $pdo->query("UPDATE club_cards SET status_card='inuse'");
+            $_SESSION['balance'] = $name[0]['balance'];
+            if ($name)
+            {
+                header('Location: ComeInAquaPark.php');
+            }
         }
     }
-    else
-    {
-        echo 'Else...';
-    }
+    echo 'Карта уже используется.';
 }
