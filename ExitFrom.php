f4eefa6904553714fd061a8e337a261248910646
Dobrodeev <dobrod@meta.ua>
2019-01-19 19:18:08 +0200

    форма и обработчик для выхода из зоны, после выхода меняется статус карты с inuse -> active

---
+++ ExitFrom.php
@@ -0,0 +1,49 @@
+<?php
+?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Come In AquaPark</title>
+        <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+    <script src="assets/jquery-3.2.1.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <!--Форма для ввода номера карты и входа в зону отдыха-->
+    <form action="#" method="post">
+        <input type="text" name="hash_card" value="" placeholder="Номер карты"><br>
+        <input type="submit" name="go" value="Come in Aqua"><br>
+        <a href="index.php">На главную</a>
+    </form>
+    </body>
+    </html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dobrodeev
+ * Date: 25.12.2018
+ * Time: 11:10
+ */
+if ($_POST['go'])
+{
+    $hash_card = $_POST['hash_card'];
+    $firstSymbol = substr($hash_card, 0, 1);
+    include 'DB.php';
+    if ($firstSymbol == 'a')
+    {
+        $stmt = $pdo->query("UPDATE hour_cards SET status_card='noactive'");
+        // разовая карта станет неактивной в любом случае, если пользователь пересидел
+        //свое время - пусть заплатит 1.5 гривны за минуту. Записать деньги в статистику
+    }
+    elseif ($firstSymbol == 'b')
+    {
+        $stmt = $pdo->query("UPDATE club_cards SET status_card='active'");
+        // если пользователь пробыл дольше положенного, пусть запишется минус на карту
+        // в БД сделать UPDATE club_cards SET balance = X
+    }
+}
+?>
