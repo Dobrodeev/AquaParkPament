b0247b3230ec5e2558dbec0010e7c3ddf3983626
Dobrodeev <dobrod@meta.ua>
2019-01-22 15:09:14 +0200

    исправили название кнопки с вход на exit, добавили еще 1 input для ввода времени, которе провел посетитель в зоне

--- ExitFrom.php
+++ ExitFrom.php
@@ -16,7 +16,8 @@
     <!--Форма для ввода номера карты и входа в зону отдыха-->
     <form action="#" method="post">
         <input type="text" name="hash_card" value="" placeholder="Номер карты"><br>
-        <input type="submit" name="go" value="Come in Aqua"><br>
+        <input type="text" name="time" value="" placeholder="превышение времени"><br>
+        <input type="submit" name="go" value="Exit"><br>
         <a href="index.php">На главную</a>
     </form>
     </body>
@@ -35,15 +36,24 @@ if ($_POST['go'])
     include 'DB.php';
     if ($firstSymbol == 'a')
     {
-        $stmt = $pdo->query("UPDATE hour_cards SET status_card='noactive'");
+        $table = 'hour_cards';
+//        $stmt = $pdo->query("UPDATE hour_cards SET status_card='noactive'");
         // разовая карта станет неактивной в любом случае, если пользователь пересидел
         //свое время - пусть заплатит 1.5 гривны за минуту. Записать деньги в статистику
     }
-    elseif ($firstSymbol == 'b')
+    else
     {
-        $stmt = $pdo->query("UPDATE club_cards SET status_card='active'");
+        $table = 'club_cards';
+//        $stmt = $pdo->query("UPDATE club_cards SET status_card='active'");
         // если пользователь пробыл дольше положенного, пусть запишется минус на карту
         // в БД сделать UPDATE club_cards SET balance = X
     }
+    $queryTime = "SELECT time_card FROM $table WHERE hash_card='$hash_card'";
+    $stmt = $pdo->query($queryTime);
+    /** @var
+     * Подсчет пересиженого времени$queryUpdate
+     */
+    $queryUpdate = "UPDATE $table SET status_card='noactive'";
+    $stmt = $pdo->query($queryUpdate);
 }
 ?>
